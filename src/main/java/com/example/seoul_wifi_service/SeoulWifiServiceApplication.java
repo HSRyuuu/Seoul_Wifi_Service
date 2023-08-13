d2ee6ff82bb2f061207b955bb3dae441b5b8b312
HyunSik Ryu <hsryu123@naver.com>
2023-08-13 17:02:09 +0900

    Initial commit

---
+++ src/main/java/com/example/seoul_wifi_service/SeoulWifiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.seoul_wifi_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulWifiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeoulWifiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seoul_wifi_service/SeoulWifiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seoul_wifi_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulWifiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
