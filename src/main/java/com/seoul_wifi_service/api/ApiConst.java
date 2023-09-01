f8e016a61dcea2f819c70e6f808486db259edf51
HyunSik Ryu <hsryu123@naver.com>
2023-09-01 14:55:02 +0900

    변수명 수정, controller 패키지 생성

--- src/main/java/com/seoul_wifi_service/api/ApiConst.java
+++ src/main/java/com/seoul_wifi_service/api/ApiConst.java
@@ -5,7 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 public abstract class ApiConst {
     public static final String SERVER_URL = "http://openapi.seoul.go.kr:8088";
 
-    @Value("${seoul.wifi.api.key}")
-    public static final String AUTHENTICATION_KEY="";
+    @Value("${seoulWifi.apiKey}")
+    public static String AUTHENTICATION_KEY;
+
     public static final String SERVICE_NAME = "TbPublicWifiInfo";
 }
